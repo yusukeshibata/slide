5c7b56bc1ce8b82f2577129ca5a80f3e814ea3bb
Yusuke Shibata <shibata@fata.io>
2016-07-27 17:46:52 +0900

    escape bugfix

--- src/actions/slide.js
+++ src/actions/slide.js
@@ -4,8 +4,15 @@ import async from 'async'
 export const SLIDE_FETCH = 'SLIDE_FETCH'
 
 export function fetch(req) {
+	let datastr = document.getElementById('data').text
+	let chars = {
+		'&gt;':'>',
+		'&lt;':'<',
+		'&amp;':'&',
+		'&quot;':'"'
+	};
 	return {
 		type: SLIDE_FETCH,
-		data: JSON.parse(document.getElementById('data').text)
+		data: JSON.parse(datastr.replace(/\&(gt|lt|amp|quot);/g,m => chars[m]))
 	}
 }
